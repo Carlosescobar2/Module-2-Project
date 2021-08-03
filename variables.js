21b3c1916b75804df3dc497d141a50744b8c8cd8
Carlos Escobar <escobarcarlos@gmail.com>
2021-08-02 22:18:42 -0600

    New-variables

--- variables.js
+++ variables.js
@@ -2,49 +2,44 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+let myname = "Carlos"
 
-myName = {
-  name: "Carlos"
-}
-console.log(myName)
+console.log(myname)
+
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
-faveNum = { 
-  num: "11 is my favorite number"
-}
+let faveNum = " 11"
 console.log(faveNum)
-//////////////////PROBLEM 3////////////////////
-/* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+// //////////////////PROBLEM 3////////////////////
+// /* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+// //CODE HERE
 let lovesCode = true; 
 console.log(lovesCode)
 
-//////////////////PROBLEM 4////////////////////
-/*
-  Write an if-statement that checks if the faveNum variable is 
-  equal to 13. If it is, console.log('lucky 13!'). 
-  If it's not, console.log('not lucky 13').
-*/
+// //////////////////PROBLEM 4////////////////////
+// /*
+//   Write an if-statement that checks if the faveNum variable is 
+//   equal to 13. If it is, console.log('lucky 13!'). 
+//   If it's not, console.log('not lucky 13').
+// */
 
-//CODE HERE
-// let Favenum = 11; 
+// //CODE HERE
 
-// if(Favenum >= 13){ 
-//   console.log("lucky 13")
-// } else{ 
-//   console.log("not lucky 13")
-// }
+if(faveNum === 13) { 
+  console.log("lucky 13")
+} else console.log("not lucky 13!")
 
-//////////////////PROBLEM 5////////////////////
-/*
-  Write a for loop that prints out your faveNum 5 times.
-*/
+// //////////////////PROBLEM 5////////////////////
+// /*
+//   Write a for loop that prints out your faveNum 5 times.
+// */
 
-//CODE HERE 
+// //CODE HERE 
+
 faveNum = [3, 3, 3 , 3, 3  ] 
 for( let i = 0; i < faveNum.length; i++){ 
 faveNum[i]+= 1
